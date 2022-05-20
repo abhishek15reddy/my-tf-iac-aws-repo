4a4a14dc237c2bdf26fde2f7375a4d0dbe5edcdb
abhishek15reddy <52439600+abhishek15reddy@users.noreply.github.com>
2022-05-20 17:12:22 +0530

    Update variables.tf
    updated key pair

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     =  "myJune2021Key"
+  default     =  "Test_Jenkins"
 }
 
 variable "instance_type" {
